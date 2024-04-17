704a2f12ec951992c58d7ad9cf72fa8a12d52f5d
Tirupathi Rao <125377941+tirupathi2219@users.noreply.github.com>
2024-04-17 05:46:22 +0000

    lg-validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,23 @@
+import { validation } from "../utils/validation"
 import Header from "./Header"
-
+import { useRef } from "react"
  const Login = () => {
+  const email=useRef(null)
+  const password=useRef(null)
+ const handleLogin = () => {
+   const val = validation(email, password)
+   console.log(val)
+ }
     return <div>
    <div>
       <Header/>
         <img src='https://assets.nflxext.com/ffe/siteui/vlv3/c1366fb4-3292-4428-9639-b73f25539794/3417bf9a-0323-4480-84ee-e1cb2ff0966b/IN-en-20240408-popsignuptwoweeks-perspective_alpha_website_medium.jpg' alt='bg' />
       </div>
-      <div className="absolute">
-      <form >
-    <input type='text' placeholder='Email Address' className='p-2 m-2'/>
-    <input type='password' placeholder='Password' className='p-2 m-2'/>
-
+      <div className="absolute bg-sky-900">
+      <form onSubmit={(e) => {e.preventDefault()}} >
+    <input ref={email} type='text' placeholder='Email Address' className='p-2 m-2'/>
+    <input ref={password} type='password' placeholder='Password' className='p-2 m-2'/>
+    <button type='submit' onClick={handleLogin}>Submit</button>
     </form>
     </div>
     </div>
---
+++ src/utils/validation.js
@@ -0,0 +1,7 @@
+export const validation = (email, password) => {
+    const emailVal = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/.test(email.current.value)
+    const passwordVal = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password.current.value)
+    if(!emailVal) return 'Email not valid';
+    if(!passwordVal) return "Password not valid"
+    return null
+}
