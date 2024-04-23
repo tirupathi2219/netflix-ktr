f8ae3ef2ff357399a19eeafa4d475f05be750768
tirupathi2219 <tirupathi2219@gmail.com>
2024-04-23 21:19:00 +0530

    '123'

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,21 +11,23 @@ const Login = () => {
     console.log(val)
     setError(val)
   }
-  return <div>
-    <div>
-      <Header />
+  return <div className="">
+    <Header />
+    <div className="absolute">
       <img src='https://assets.nflxext.com/ffe/siteui/vlv3/c1366fb4-3292-4428-9639-b73f25539794/3417bf9a-0323-4480-84ee-e1cb2ff0966b/IN-en-20240408-popsignuptwoweeks-perspective_alpha_website_medium.jpg' alt='bg' />
     </div>
-    <div className="absolute bg-stone-800">
+    
+    <form onSubmit={(e) => { e.preventDefault() }} className=" w-4/12 absolute my-36 mx-auto left-0 right-0 bg-black text-center bg-opacity-80">
+      <h1 className="text-white">{isSignUpForm ? 'Sign Up' : 'Sign In'}</h1>
       {error && <p> {error} </p>}
-      <form onSubmit={(e) => { e.preventDefault() }} >
-        {isSignUpForm && <input type="text" placeholder="Full Name" className='p-2 m-2 block rounded-md' />}
-        <input ref={email} type='text' placeholder='Email Address' className='p-2 m-2 block rounded-md' />
-        <input ref={password} type='password' placeholder='Password' className='p-2 m-2 block rounded-md' />
-        <button type='submit' className="p-2 m-2 block bg-green-200 rounded-md" onClick={handleLogin}>Submit</button>
-        <p className="text-white" onClick={() => {setIsSignUpForm(!isSignUpForm)}}> {isSignUpForm ? "Already Have Account SignIn" : "Dont Have Account SignUp"}</p>
-      </form>
-    </div>
+      {isSignUpForm && <input type="text" placeholder="Full Name" className='p-2 m-2 block rounded-md bg-gray-800 w-full' />}
+      <input ref={email} type='text' placeholder='Email Address' className='p-2 m-2 block rounded-md bg-gray-800 w-full' />
+      <input ref={password} type='password' placeholder='Password' className='p-2 m-2 block rounded-md bg-gray-800 w-full ' />
+      <div>
+        <button type='submit' className="p-2 bg-red-800 w-full rounded-md" onClick={handleLogin}>Submit</button>
+      </div>
+      <p className="text-white" onClick={() => { setIsSignUpForm(!isSignUpForm) }}> {isSignUpForm ? "Already Have Account SignIn" : "Dont Have Account SignUp"}</p>
+    </form>
   </div>
 }
 export default Login
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDqpJYVIepqNsHeqXwOZ_gmw3S1Cc8v6sw",
+  authDomain: "netflix-ktr.firebaseapp.com",
+  projectId: "netflix-ktr",
+  storageBucket: "netflix-ktr.appspot.com",
+  messagingSenderId: "44135959133",
+  appId: "1:44135959133:web:7db566de4018e86f30dbb4",
+  measurementId: "G-SJ2J8RK629"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
