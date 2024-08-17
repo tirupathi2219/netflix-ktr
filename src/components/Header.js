6780e90f6be5f0d9fdb72a664986098bf9279620
vijay-ui-ux <vijaykchandan@gmail.com>
2024-08-17 02:24:31 -0500

    'sign-out-edit'

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,11 @@ import { signOut } from 'firebase/auth';
 import React from 'react'
 import { auth } from '../utils/firebase';
 import { updateUserData } from '../redux/slice/user';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const  user = useSelector((state) => state.user.user )
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleUserSignIn = () => {
@@ -22,10 +23,13 @@ const Header = () => {
 
     <div className='absolute px-8 py-2 bg-gradient-to-b from-black w-full flex justify-between'>
       <img className='w-44' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='logo' />
+  {
+    user && 
     <div>
       
     <button onClick={handleUserSignIn} className='bg-sky-600'> Sign out </button>
     </div>
+  }  
     </div>
   )
 }
